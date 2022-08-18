67d564e17739f326ccab3d701834086bea829411
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2022-08-18 16:05:30 -0400

    Add more structure to the UI
    This improves the GameUI, and fixes the name error in InfoDisplay

--- src/components/GameComponents/GameInterface/GameUI.js
+++ src/components/GameComponents/GameInterface/GameUI.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { SectionTitle } from '../../Sections/SectionTitle'
 import { Board } from './Board'
-import { InfoDisplay } from './InforDisplay'
+import { InfoDisplay } from './InfoDisplay'
+import { PlayerList } from './PlayerList'
 
-export const GameUI = () => {
+export const GameUI = ({game, close}) => {
+    const title = game.title;
+    const mode = game.gameMode;
+    const sData = game.shipData;
+    const list = game.dataList;
+    const dispFunc = game.listDisplay;
+    const players = game.Players;
+    const cPlayer = game.cPlayer;
+    const local = game.local;
+    const updatePlayer = game.updatePlayer;
+    const active = game.active;
+    const stage = game.stage;
+    const impulse = game.impulse;
+    const iCount = game.impulseCount;
+
     return (
         <div className="game">
-        <SectionTitle title={`Game`} className="gameTitle" minimizable={false} close={() => console.log("Leave")} />
+        <SectionTitle title={`${mode}: ${title}`} className="gameTitle" minimizable={false} close={close} />
         <Board grid={estGrid} main={estMain} press={movCursTo} />
         <InfoDisplay title="Information" className="Info" information={sData} />
-        <InfoDisplay title="ShipList" className="ShipList" information={list} selected={cMenu} displayFunction={shipList} />
-        <div className="Players">
-            {mainGame.local ? players.map((player, index) => {
-                return <EditablePlayer inGame={players} player={player} key={player.playerNum} 
-                active={index === cPlayer} updatePlayer={playerInfo => updateplayer(playerInfo, index)}/>
-            }):(<><EditablePlayer inGame={players} player={players[cPlayer]} 
-                updatePlayer={(playerInfo => updateplayer(playerInfo, cPlayer))} />
-            {mainGame.Players.map((player, index) => {
-                console.log(player);
-                if (index === cPlayer) return <></>;
-                return <Section title={player.Name} close={false}>Has Moved: {player.hasMoved ? "True":"False"}</Section>
-            })}</>)}
-        </div>
-        { active ? <ButtonGrid system={systemFunctions} move={movCurs} stage={stage} />:
-        <InfoDisplay title="Players" className="IO" information={
-            players.map((player) => `${player.Name}: ${player.hasMoved ? `Ready`: `Not Ready`}`)} />}
+        <InfoDisplay title="ShipList" className="ShipList" information={list} selected={cMenu} displayFunction={dispFunc} />
+        <PlayerList players={players} cPlayer={cPlayer} local={local} updatePlayer={updatePlayer} />
+        { active ? <ButtonGrid system={systemFunctions} move={movCurs} stage={stage} />:<></>}
         <Footer stage={stage} impulse={impulse} impulseCount={iCount} />
     </div>
     )
