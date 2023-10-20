4b9a06dc0b3a6d8b41623a979da22b49a806ef74
Syndicationa <93161645+Syndicationa@users.noreply.github.com>
2023-10-20 13:58:59 -0400

    Adds a way for tabs to be changed externally

--- src/components/GameComponents/GameInterface/Tabs.js
+++ src/components/GameComponents/GameInterface/Tabs.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { minMax } from '../../../functions/functions.mjs';
 
-export const Tabs = ({childArr, nameArr}) => {
-    const [selected, setSelected] = useState(0);
-  return (
-    <div className='Tabs'>
-        <div className='TabList'>
-            {nameArr.map((name, i) => {
-                return (<button onClick={() => setSelected(i)} key={i}>{name}</button>)
-            })}
+export const Tabs = ({childArr, nameArr, selection = 0}) => {
+    const [selected, setSelected] = useState(selection);
+
+    useEffect(() => {
+        const select = minMax(selection, 0, nameArr.length - 1);
+        setSelected(select);
+    }, [selection, nameArr.length]);
+
+    return (
+        <div className='Tabs'>
+            <div className='TabList'>
+                {nameArr.map((name, i) => {
+                    return (<button onClick={() => setSelected(i)} key={i}>{name}</button>)
+                })}
+            </div>
+            {childArr[selected]}
         </div>
-        {childArr[selected]}
-    </div>
-  )
+    )
 }
